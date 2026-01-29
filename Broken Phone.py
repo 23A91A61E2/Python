45be91c98122a4285640db2f39ba9f73ab7b695e
Varshitha <147410265+23A91A61E2@users.noreply.github.com>
2026-01-29 22:04:53 +0530

    Create Broken Phone.py

---
+++ Broken Phone.py
@@ -0,0 +1,9 @@
+t=int(input())
+for _ in range(t):
+    x,y=map(int,input().split())
+    if x<y:
+        print('REPAIR')
+    elif x>y:
+        print('NEW PHONE')
+    else:
+        print('ANY')
